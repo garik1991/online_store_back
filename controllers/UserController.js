e211e2f7bebb0c969e13848decdf1484ccf69535
Albert Hovhannisian <garik.melqonyan1991@gmail.com>
2022-07-18 18:47:01 +0400

    zdxde

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,7 +1,22 @@
 const ApiError = require('../error/ApiError')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const {User, Basket} = require('../models/models')
+
 
 class UserController{
-    async registration(req, res){
+    async registration(req, res, next){
+        const {email, password, role} = req.body
+        if(!email || !password){
+            return next(ApiError.internal('Incorrect data'))
+        }
+        const candidate = await User.findOne({where: {email}});
+        if(candidate){
+            return next(ApiError.internal('User with such email already exists'))
+        }
+        const hashedPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({email, password: hashedPassword, role})
+        const basket = await Basket.create({basketId: basket.id})
 
     }
 
